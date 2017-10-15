23930724f004a8e4af41d328f8bb1c7daabf44f3
Xiangyang Ju <xiangyang.ju@gmail.com>
2017-10-14 19:06:50 -0700

    add locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,20 @@
 var addressPoints = [
   [
+    "Shandong, China",
+    36.0000001,
+    118.9999999
+  ],
+  [
+    "DuPage County, IL, US",
+    41.8603735,
+    -88.0906873
+  ],
+  [
+    "Geneva, Switzerland",
+    46.2017559,
+    6.1466014
+  ],
+  [
     "Beijing, China",
     39.9059631,
     116.391248
@@ -15,9 +30,19 @@ var addressPoints = [
     2.1874811
   ],
   [
-    "Geneva, Switzerland",
-    46.2017559,
-    6.1466014
+    "Munich, German",
+    48.1352414,
+    11.5821626
+  ],
+  [
+    "Nanjing, China",
+    32.0609736,
+    118.7916458
+  ],
+  [
+    "Lausanne, Switzerland",
+    46.5218269,
+    6.6327025
   ],
   [
     "Lemont, IL, US",
